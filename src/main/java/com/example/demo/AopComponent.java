a1361fd799b3d4f2bedd484bb8199fd914729acf
Duy-Pham <Duy.Pham@ibm.com>
2021-07-28 09:12:31 +0900

    add Around advice

--- src/main/java/com/example/demo/AopComponent.java
+++ src/main/java/com/example/demo/AopComponent.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 /* execution: 実行
  * execution(String *..*MainController.input*(..))
+ * execution(String *..*)
  * within: 指定の型やパッケージで制限
  * within(com.example.demo.MainController)
  * 
@@ -27,5 +30,15 @@ public class AopComponent {
 		String className = jp.getSignature().getDeclaringType().getSimpleName();
 		String methodName = jp.getSignature().getName();
 		System.out.println(className +"クラスの"+ methodName +"メソッドを終了します。");
+	}
+	
+	@Around("within(com.example.demo.MainController)")
+	public Object around(ProceedingJoinPoint pjp) throws Throwable {
+		String className = pjp.getSignature().getDeclaringType().getSimpleName();
+		String methodName = pjp.getSignature().getName();
+	    System.out.println(className +"クラスの"+ methodName + "メソッド around - before !!");   // 前処理
+	    Object retVal = pjp.proceed();                            
+	    System.out.println(className +"クラスの"+ methodName + "メソッド around - after !!");    // 後処理
+	    return retVal;
 	}
 }
--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -14,15 +14,18 @@ public class MainController {
 	
 	@GetMapping("/test1")
 	public String input1() {
-		return "test1";
+		System.out.println("input1メソッドを実行します。");
+		return "test-abc";
 	}
 	@GetMapping("/test2")
 	public String input2() {
+		System.out.println("input2メソッドを実行します。");
 		return "test2";
 	}
 	
 	@GetMapping("/test-jpa")
 	public List<Member> get(){
+		System.out.println("getメソッドを実行します。");
 		return memberRepository.findAll();
 	}
 }
